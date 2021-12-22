b59461daf6aebf7f2da857b8824271f16dab2198
Puvan Raj Mohan <>
2021-12-22 21:24:52 +0800

    Sider appears for polls and forum pages

---
+++ src/components/AppSider/AppSider.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Layout } from "antd";
+import './style.css'
+
+const { Sider } = Layout;
+
+export const AppSider = () => {
+  return (
+    <Sider>
+      <div className="filler"></div>
+    </Sider>
+  )
+}
