f25844c1280aae0ee681a3ea18f986d8574dd87c
Puvan Raj Mohan <>
2021-12-23 04:09:12 +0800

    Add Counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -19,13 +19,13 @@ export const Counter = ({ id, counterName, counterValue, updateCounter }) => {
     <>
       <Row justify="center" align="middle">
         <Col>
-          <div className="stupid">{`${counterName}: ${counterValue}`}</div>
+          <div className="counter-name">{`${counterName}: ${counterValue}`}</div>
         </Col>
       </Row>
       <Row justify="center" align="middle">
         <Col>
-          <Button shape="circle" icon={<UpOutlined />} size="small" className="counter-button" onClick={() => editCounterValue(true)}></Button>
-          <Button shape="circle" icon={<DownOutlined />} size="small" className="counter-button" onClick={() => editCounterValue(false)}></Button>
+          <Button className="counter-button" shape="circle" icon={<UpOutlined />} size="small" onClick={() => editCounterValue(true)}></Button>
+          <Button className="counter-button" shape="circle" icon={<DownOutlined />} size="small" onClick={() => editCounterValue(false)}></Button>
         </Col>
       </Row>
     </>
--- src/components/CounterContainer/CounterContainer.js
+++ src/components/CounterContainer/CounterContainer.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import { Button, Row, Col } from 'antd';
+import { PlusOutlined } from '@ant-design/icons'
 import * as GeneralHelper from '../../helper/GeneralHelper'
 
 import { CounterContext } from 'context/CounterContext'
 
 import { Counter } from 'components/Counter/Counter'
+
+import './style.css'
 
 export const CounterContainer = () => {
   const [counters, setCounters] = useState(() => JSON.parse(window.localStorage.getItem('counters')) || [{id: 'test', counterName:'Test Name', counterValue:3}])
@@ -16,6 +20,17 @@ export const CounterContainer = () => {
     GeneralHelper.setLocalStorage('counters', countersCopy)
   }
 
+  const addCounter = () => {
+    const newCounter = {
+      id: GeneralHelper.generateId(8),
+      counterName: 'New Counter',
+      counterValue: 0
+    }
+    const newCountersArray = [...counters, newCounter]
+    setCounters(newCountersArray)
+    GeneralHelper.setLocalStorage('counters', newCountersArray)
+  }
+
   return (
     <div>
       <CounterContext.Provider value={{counters, setCounters}}>
@@ -23,6 +38,11 @@ export const CounterContainer = () => {
           return <Counter key={counter.id} id={counter.id} counterName={counter.counterName} counterValue={counter.counterValue} updateCounter={updateCounter}/>
         })}
       </CounterContext.Provider>
+      <Row justify="center" align="middle">
+        <Col>
+          <Button className="add-counter-button" icon={<PlusOutlined />} onClick={() => addCounter()}>Add Counter</Button>
+        </Col>
+      </Row>
     </div>
   )
 }
