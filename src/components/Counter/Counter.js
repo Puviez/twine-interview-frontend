b5b6bf42c1a0a3be44852555d46bd51de0218c49
Puvan Raj Mohan <>
2021-12-23 10:21:06 +0800

    Delete Counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Button, Row, Col } from 'antd';
-import { UpOutlined, DownOutlined } from '@ant-design/icons'
+import { UpOutlined, DownOutlined, DeleteOutlined } from '@ant-design/icons'
 
 import './style.css'
 
-export const Counter = ({ id, counterName, counterValue, updateCounter }) => {
+export const Counter = ({ id, counterName, counterValue, updateCounter, removeCounter }) => {
   const editCounterValue = (isIncrement) => {
     const newCounterValue = isIncrement ? counterValue + 1 : counterValue - 1
     console.log(newCounterValue)
@@ -19,14 +19,26 @@ export const Counter = ({ id, counterName, counterValue, updateCounter }) => {
     <>
       <Row justify="center" align="middle">
         <Col>
-          <div className="counter-name">{`${counterName}: ${counterValue}`}</div>
+          <Row justify="center" align="middle">
+            <Col>
+              <div className="counter-name">{`${counterName}: ${counterValue}`}</div>
+            </Col>
+          </Row>
+          <Row justify="center" align="middle">
+            <Col>
+              <Button className="counter-button" shape="circle" icon={<UpOutlined />} size="small" onClick={() => editCounterValue(true)}></Button>
+              <Button className="counter-button" shape="circle" icon={<DownOutlined />} size="small" onClick={() => editCounterValue(false)}></Button>
+            </Col>
+          </Row>
         </Col>
-      </Row>
-      <Row justify="center" align="middle">
         <Col>
-          <Button className="counter-button" shape="circle" icon={<UpOutlined />} size="small" onClick={() => editCounterValue(true)}></Button>
-          <Button className="counter-button" shape="circle" icon={<DownOutlined />} size="small" onClick={() => editCounterValue(false)}></Button>
-        </Col>
+            <Row>
+              <Button className="counter-button" shape="circle" icon={<DeleteOutlined />} size="small" onClick={() => removeCounter()}></Button>
+            </Row>
+            <Row>
+              <Button className="counter-button" shape="circle" icon={<DeleteOutlined />} size="small" onClick={() => removeCounter()}></Button>
+            </Row>
+          </Col>
       </Row>
     </>
   )
--- src/components/CounterContainer/CounterContainer.js
+++ src/components/CounterContainer/CounterContainer.js
@@ -12,14 +12,6 @@ import './style.css'
 export const CounterContainer = () => {
   const [counters, setCounters] = useState(() => JSON.parse(window.localStorage.getItem('counters')) || [{id: 'test', counterName:'Test Name', counterValue:3}])
 
-  const updateCounter = (updatedCounter) => {
-    const countersCopy = [...counters]
-    const index = countersCopy.findIndex(counter => counter.id === updatedCounter.id)
-    countersCopy.splice(index, 1, updatedCounter)
-    setCounters(countersCopy)
-    GeneralHelper.setLocalStorage('counters', countersCopy)
-  }
-
   const addCounter = () => {
     const newCounter = {
       id: GeneralHelper.generateId(8),
@@ -31,11 +23,28 @@ export const CounterContainer = () => {
     GeneralHelper.setLocalStorage('counters', newCountersArray)
   }
 
+  const updateCounter = (updatedCounter) => {
+    const countersCopy = [...counters]
+    const index = countersCopy.findIndex(counter => counter.id === updatedCounter.id)
+    countersCopy.splice(index, 1, updatedCounter)
+    setCounters(countersCopy)
+    GeneralHelper.setLocalStorage('counters', countersCopy)
+  }
+  
+  // I considered combining the update and remove counter functions into a single one, but I think keeping separate functions for each task is better for maintainabillity and readability
+  const removeCounter = (id) => {
+    const countersCopy = [...counters]
+    const index = countersCopy.findIndex(counter => counter.id === id)
+    countersCopy.splice(index, 1)
+    setCounters(countersCopy)
+    GeneralHelper.setLocalStorage('counters', countersCopy)
+  }
+
   return (
     <div>
       <CounterContext.Provider value={{counters, setCounters}}>
         {counters.map((counter) => {
-          return <Counter key={counter.id} id={counter.id} counterName={counter.counterName} counterValue={counter.counterValue} updateCounter={updateCounter}/>
+          return <Counter key={counter.id} id={counter.id} counterName={counter.counterName} counterValue={counter.counterValue} updateCounter={updateCounter} removeCounter={removeCounter}/>
         })}
       </CounterContext.Provider>
       <Row justify="center" align="middle">
